d5516c04de0309f862d07dd4b6f5a561035e6ad9
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-01-20 00:56:27 +0530

    Nested Loops

---
+++ Conditionals and Loops/nestedloop.py
@@ -0,0 +1,11 @@
+# Nested for Loop - refers to a for loop inside a for loop
+
+List1=[1,2,3]
+List2=[4,5,6]
+
+for i in List1:
+     for j in List2:
+          print(i, j)
+     print()
+          
+          
