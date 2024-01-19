38faac4fe7066054a76404f233e04684b06b8b80
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-01-20 00:33:47 +0530

    loops

---
+++ Conditionals and Loops/forloops_else.py
@@ -0,0 +1,18 @@
+# Introduction - The else block will be executed only when the loop is NOT Terminated abruptly by the break keyword
+
+# SYNTAX - FOR VAR IN SEQUENCE:
+             #STATEMENTS INSIDE FOR
+           #ELSE:
+            #STATEMENTS INSIDE ELSE
+            
+# USE OF FOR LOOP WITH ELSE BLOCK 
+
+Languages=['Python','Rust','C','C++']
+
+for Lang in Languages:
+      if 'Python'==Lang:
+           print("I like Python")
+           break
+      else:
+          print("I dont like Python")
+ 
