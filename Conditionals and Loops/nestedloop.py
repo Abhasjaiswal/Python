b8e474fad7e94191d9bc7b2a6c20000b914b9a1a
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-01-20 09:15:15 +0530

    Nested Loops

--- Conditionals and Loops/nestedloop.py
+++ Conditionals and Loops/nestedloop.py
@@ -8,4 +8,16 @@ for i in List1:
           print(i, j)
      print()
           
-          
+# Nested While Loop- It is a while loop within a while loop
+List1=[1,2,3]
+List2=[4,5,6]
+
+i=0
+j=0
+while i<len(List1):
+      while j<len(List2):
+            print(List1[i],List2[j])
+            j+=1
+      print()
+      i+=1
+      
