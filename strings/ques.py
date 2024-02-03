9aaf859689ef3d8f8d2f93d9c1f7c7a24eb32414
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-02-03 09:55:53 +0530

    strings

---
+++ strings/ques.py
@@ -0,0 +1,19 @@
+s='strawberry'
+idx=1
+while idx<len(s):
+     print(s[idx],end="")
+     idx+=2
+print()
+s='green grapes'
+for ch in s:
+     print('HELLO')
+     
+# P : 0
+# Y : 1
+# T : 2
+# H : 3
+# O : 4
+# N : 5
+
+str='PYTHON'
+print(str[1:20])
--- strings/stringtraversal.py
+++ strings/stringtraversal.py
@@ -4,4 +4,28 @@ for i in message:
      print("message[", index, "]= ",i)
      index+=1
      
+# string traversal using for loop
+
+A='Welcome'
+for i in A:
+     print(i)
+
+
+A='Welcome'
+i=0
+while i<len(A):
+     print(A[i])
+
+
+fruit='fruit'
+index=0
+while index<len(fruit):
+     letter=fruit[index]
+     print(letter)
+     index+=1
      
+fruit="peach"
+for char in fruit:
+     print(char)
+
+
