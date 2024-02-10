ca73c78fe08714b393a21ae538bf87845919eaaf
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-02-10 10:27:13 +0530

    updated regular exp

--- strings/regx.py
+++ strings/regx.py
@@ -14,3 +14,95 @@ print(match)
 match = re.search(r'\.',s)
 print(match)
 
+txt='The rain in Spain'
+
+#find all lower case characters alphabetically between "a" and "m"
+x=re.findall("[a-m]",txt)
+print(x)
+
+txt1="hello planet"
+# search for a sequence that starts with "he" followed by two(any) characters and an "o":
+x = re.findall("he..o", txt1)
+print(x)
+
+#checks if the string ends with 'planet'
+x=re.findall("planet$",txt1)
+print(x)
+
+#search for a sequence that starts with "he" followed by O or more(any) character, and an "o"
+
+x=re.findall("he.*o",txt1)
+print(x)
+
+text="That will be 59 dollars"
+
+# find all digit characters
+x=re.findall("\d",text)
+print(x)
+
+#search for a sequence that starts with "he", followed exactly 2(any) characters, and an "o":
+x=re.findall("he.{2}o",txt1)
+print(x)
+
+#search for a sequence that starts with "he", followed by 1 or more (any) characrers,and an "o"
+x=re.findall("he.+o",txt1)
+print(x)
+
+new='The rain in Spain falls mainly in the plain!'
+#check if the string contains either "falls" or "stays":
+x=re.findall("falls|stays",new)
+print(x)
+
+if x:
+     print("Yes, there is atleast one match")
+else:
+     print("No Match")
+
+sentence='The rain in spain'
+
+#check if the string starts with "The":
+x=re.findall("\AThe",sentence)
+print(x)
+
+if x:
+      print("Yes,there's a match")
+else:
+      print("No Match")
+
+#check if "ain" is present at the beginning of a word:
+
+x=re.findall(r"\bain",sentence)
+print(x)
+
+if x:
+      print("Yes,there is at least one match")
+else:
+      print("No Match")
+
+#check if "ain" is present at the end of a word:
+x=re.findall(r"ain\b",sentence)
+print(x)
+
+if x:
+      print("Yes,there's at least one match")
+else:
+      print("No Match")
+
+#check if "ain" is present but NOT at the end of a word:
+
+x=re.findall(r"ain\B",sentence)
+print(x)
+
+if x:
+      print("Yes,there is at least one match")
+      
+else:
+      print("No Match")
+      
+x=re.findall("\d",sentence)
+print(x)
+if x:
+      print("Yes,there is at least one match")
+      
+else:
+      print("No Match")
