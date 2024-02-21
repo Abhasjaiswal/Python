6a041e90f1d77ef807f468139c5c39d32b729b6f
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-02-22 01:17:42 +0530

    File handling

---
+++ File-IO/basics.py
@@ -0,0 +1,6 @@
+#Python supports file handling and allows users to handle files i.e., to read and write files, along with many other file handling options, to operate on files. 
+#for example
+f=open('/Users/abhasjaiswal/Desktop/Python/File-IO/file.txt','r')
+data=f.read()
+print(data)
+f.close()
