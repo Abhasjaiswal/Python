8437ee29cefa4f4aaa8744278c551f50d25d88b0
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-02-29 10:30:54 +0530

    Updated Lists

--- Lists/question.py
+++ Lists/question.py
@@ -21,3 +21,32 @@ L=['a','b',['cc','dd',['eee','fff']],'g','h']
 print(L[2])
 print(L[2][2])
 print(L[2][2][0])
+
+
+#Change Nested List Item value
+L=['a',['bb','cc'],'d']
+L[1][1]=0
+print(L)
+
+# remove items from a nested list
+L=['a',['bb','cc','dd'],'e']
+x=L[1].pop(1)
+print(L)
+print(x)
+
+N=['a',['a','1'],['b','2']]
+x=N[1].pop(1)
+print(N)
+print(x)
+
+
+List1 = ['a']
+List2 = ['b']
+List2 = List1
+
+print(List1)  
+print(List2) 
+
+List1.append('c')
+print(List1) 
+print(List2)  
