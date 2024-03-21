698487247ebde4bcf847d871a82622af1199653c
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-03-21 10:11:50 +0530

    updates

---
+++ Dicitionary/dict5.py
@@ -0,0 +1,18 @@
+Dict={'Roll_No':'16/001','Name':'Arnav','Course':'B.tech'}
+print(sorted(Dict.keys()))
+
+print('KEYS: ',end= ' ')
+for key in Dict:
+    print(key,end=' ')
+print("\n VALUES: ",end=' ')
+
+for val in Dict.values():
+     print(val,end=' ')
+print("\nDICTIONARY: ",end=' ')
+for key, val in Dict.items():
+     print(key,val,'\t',end=' ')
+     
+students={'Shiv':{'CS':90,'DS':89,'CSA':'92'},
+          'Rashika':{'CS':100,'DS':100,'CSA':100}}
+for key,val in students.items():
+     print(key,val)
